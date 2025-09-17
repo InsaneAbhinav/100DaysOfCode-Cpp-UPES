9381739f75d5b43c08048b588418a69dcf38ea73
Abhinav Singla <142439429+InsaneAbhinav@users.noreply.github.com>
2025-09-17 23:39:11 +0530

    Update Q2.c

--- Day 4/Q2.c
+++ Day 4/Q2.c
@@ -1,18 +1,17 @@
 // Q2: Write a program to find and display the sum of the first n natural numbers
 
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 
 int main() {
     int n;
-    cout << "Enter n: ";
-    cin >> n;
+    printf("Enter n: ");
+    scanf("%d", &n);
 
     int sum = 0;
     for (int i = 1; i <= n; i++) {
         sum += i; // Add i to sum
     }
 
-    cout << "Sum=" << sum;
+    printf("Sum=%d", sum);
     return 0;
 }
